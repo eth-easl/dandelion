416387e7ef954fff16cfa4da3cb4b05998d39394
Tom Kuchler <tom.kuchler@inf.ethz.ch>
2022-05-05 11:34:28 +0200

    Fixed ddc config, removed C64 turn off as it proved unnecessary

--- compartment/trial.c
+++ compartment/trial.c
@@ -3,22 +3,35 @@
 
 int main(int argc, char const *argv[]) {
 
-  int test_array[8] = {0,1,2,3,4,5,6,7};
-  int test_load = -1;
-  int test_move = -1;
+  int testArray[8] = {0,1,2,3,4,5,6,7};
+  long pointer = 0;
+  int loaded = -1;
+  int moved = -1;
+  int arraySample = testArray[2];
 
   // turn off capability mode
   __asm__ volatile(
-    "bx #4\n" // turn off capability mode
-    "mov %w[input], 9\n"
-    "ldr %w[load_target] ,[%w[pointer]]\n"
-    "bx #4\n" // turn on capability mode
-    : [input] "=r" (test_move),
-    [load_target] "=r" (test_load)
-    : [pointer] "r" (test_array)
+    // Not necessary as it seems they differentiates cap / pointers using
+    // pointer bit not instruction code. Which is not quite what they say in
+    // the manual.
+    // "bx 4\n" // toggle capability mode
+    "msr ddc, %x[capPointer] \n" // set capability as DDC
+    "cvtd %x[pointer], %x[capPointer]\n" // convert capability to pointer
+    "mov %w[input], 8\n" // normal move operation, should always work
+    // the default data capability is set correctly
+    "ldr %w[loadTarget], [ %x[pointer], 4 ] \n" // load operation, should only work when capabilities are enabled or
+    : [input] "+r" (moved),
+    [loadTarget] "+r" (loaded),
+    [pointer] "+r" (pointer)
+    : [capPointer] "r" (testArray)
+    : "c28"
   );
+
   // turn capability mode back on
-  printf("test_move is now: %d\n", test_move);
-  printf("test_move is now: %d\n", test_load);
+  printf("test_move is now: %d \n", moved);
+  printf("test_array is %p \n", testArray);
+  printf("test_array value is %d \n", arraySample);
+  printf("test_pointer is %p \n", pointer);
+  printf("test_load is now: %d \n", loaded);
   return 0;
 }
