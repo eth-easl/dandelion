29922e5cb0377396175c8a931a0dadb2c7d6e4ca
Tom Kuchler <tom.kuchler@inf.ethz.ch>
2022-06-24 16:02:23 +0200

    Add context clean up and fix bugs with capabilties

--- compartment/compartment.h
+++ compartment/compartment.h
@@ -66,22 +66,77 @@ static inline void storeContext(void* __capability storeCap) {
     "mrs c0, CID_EL0 \n"
     "str c0, [%x[cap], #528] \n"
     // Software Thead ID
-    "mrs c0, RCTPIDR_EL0 \n"
-    "str c0, [%x[cap], #544] \n"
     "mrs c0, CTPIDR_EL0 \n"
     "str c0, [%x[cap], #528] \n"
+    "mrs c0, RCTPIDR_EL0 \n"
+    "str c0, [%x[cap], #544] \n"
     : : [cap] "r" (storeCap) : "c0"
   );
 }
 
-static inline void restoreContext(void* __capability restoreCap)
+
+static inline void prepareContextAndJump(void* __capability ddc, void* __capability target)
   __attribute__((always_inline));
-static inline void restoreContext(void* __capability restoreCap){
+static inline void prepareContextAndJump(void* __capability ddc, void* __capability target){
+  __asm__ volatile(
+    // move target to c30 for the jump, c30 is arbitrary choice
+    "mov c30, %x[target] \n"
+    // prepare DDC:
+    "msr ddc, %x[ddc] \n"
+    // prepare stack pointer: TODO set non zero
+    "mov x0, #0 \n"
+    "cpy c0, CSP \n"
+    // zero out system registers
+    // compartment ID
+    "msr CID_EL0, c0 \n"
+    // Software Thead ID
+    "msr CTPIDR_EL0, c0 \n"
+    "msr RCTPIDR_EL0, c0 \n"
+    // zero out remaining registers
+    "mov x0, #0 \n"
+    "mov x1, #0 \n"
+    "mov x2, #0 \n"
+    "mov x3, #0 \n"
+    "mov x4, #0 \n"
+    "mov x5, #0 \n"
+    "mov x6, #0 \n"
+    "mov x7, #0 \n"
+    "mov x8, #0 \n"
+    "mov x9, #0 \n"
+    "mov x10, #0 \n"
+    "mov x11, #0 \n"
+    "mov x12, #0 \n"
+    "mov x13, #0 \n"
+    "mov x14, #0 \n"
+    "mov x15, #0 \n"
+    "mov x16, #0 \n"
+    "mov x17, #0 \n"
+    "mov x18, #0 \n"
+    "mov x19, #0 \n"
+    "mov x20, #0 \n"
+    "mov x21, #0 \n"
+    "mov x22, #0 \n"
+    "mov x23, #0 \n"
+    "mov x24, #0 \n"
+    "mov x25, #0 \n"
+    "mov x26, #0 \n"
+    "mov x27, #0 \n"
+    "mov x28, #0 \n"
+    "mov x29, #0 \n"
+    "br c30 \n"
+    : : [ddc] "r" (ddc), [target] "r" (target) :
+    "c0"
+  );
+}
+
+// expects the capability holding the context in c29
+static inline void restoreContext()
+  __attribute__((always_inline));
+static inline void restoreContext(){
     __asm__ volatile(
-      // put restore capability in C0 to overwrite it last
-      "cpy c0, %x[cap] \n"
+      "cpy c0, c29 \n"
       // Software Thead ID
-      "ldr c1, [c0, #544] \n"
+      "ldr c1, [c29, #544] \n"
       "msr RCTPIDR_EL0, c1 \n"
       "ldr c1, [c0, #528] \n"
       "msr CTPIDR_EL0, c1 \n"
@@ -92,42 +147,40 @@ static inline void restoreContext(void* __capability restoreCap){
       "ldr c1, [c0, #512] \n"
       "msr DDC, c1 \n"
       // store the stack capability
-      "ldr c1, [%x[cap], #496] \n"
+      "ldr c1, [c0, #496] \n"
       "cpy c1, CSP \n"
-
       // store the contents of all normal registers
-      "str c30, [c0, #480] \n"
-      "str c29, [c0, #464] \n"
-      "str c28, [c0, #448] \n"
-      "str c27, [c0, #432] \n"
-      "str c26, [c0, #416] \n"
-      "str c25, [c0, #400] \n"
-      "str c24, [c0, #384] \n"
-      "str c23, [c0, #368] \n"
-      "str c22, [c0, #352] \n"
-      "str c21, [c0, #336] \n"
-      "str c20, [c0, #320] \n"
-      "str c19, [c0, #304] \n"
-      "str c18, [c0, #288] \n"
-      "str c17, [c0, #272] \n"
-      "str c16, [c0, #256] \n"
-      "str c15, [c0, #240] \n"
-      "str c14, [c0, #224] \n"
-      "str c13, [c0, #208] \n"
-      "str c12, [c0, #192] \n"
-      "str c11, [c0, #176] \n"
-      "str c10, [c0, #160] \n"
-      "str c9, [c0, #144] \n"
-      "str c8, [c0, #128] \n"
-      "str c7, [c0, #112] \n"
-      "str c6, [c0, #96] \n"
-      "str c5, [c0, #80] \n"
-      "str c4, [c0, #64] \n"
-      "str c3, [c0, #48] \n"
-      "str c2, [c0, #32] \n"
-      "str c1, [c0, #16] \n"
-      "str c0, [c0, #0] \n"
-      : : [cap] "r" (restoreCap)
+      "ldr c30, [c0, #480] \n"
+      "ldr c29, [c0, #464] \n"
+      "ldr c28, [c0, #448] \n"
+      "ldr c27, [c0, #432] \n"
+      "ldr c26, [c0, #416] \n"
+      "ldr c25, [c0, #400] \n"
+      "ldr c24, [c0, #384] \n"
+      "ldr c23, [c0, #368] \n"
+      "ldr c22, [c0, #352] \n"
+      "ldr c21, [c0, #336] \n"
+      "ldr c20, [c0, #320] \n"
+      "ldr c19, [c0, #304] \n"
+      "ldr c18, [c0, #288] \n"
+      "ldr c17, [c0, #272] \n"
+      "ldr c16, [c0, #256] \n"
+      "ldr c15, [c0, #240] \n"
+      "ldr c14, [c0, #224] \n"
+      "ldr c13, [c0, #208] \n"
+      "ldr c12, [c0, #192] \n"
+      "ldr c11, [c0, #176] \n"
+      "ldr c10, [c0, #160] \n"
+      "ldr c9, [c0, #144] \n"
+      "ldr c8, [c0, #128] \n"
+      "ldr c7, [c0, #112] \n"
+      "ldr c6, [c0, #96] \n"
+      "ldr c5, [c0, #80] \n"
+      "ldr c4, [c0, #64] \n"
+      "ldr c3, [c0, #48] \n"
+      "ldr c2, [c0, #32] \n"
+      "ldr c1, [c0, #16] \n"
+      "ldr c0, [c0, #0] \n"
     );
 }
 
--- compartment/trial.c
+++ compartment/trial.c
@@ -1,13 +1,12 @@
 // include for printing
 #include <stdio.h>
 #include <sys/mman.h>
-
-// #include "sys/cheri/cheric.h"
+#include <stdlib.h>
 
 #include "compartment.h"
 
 int main(int argc, char const *argv[]) {
-  __label__ returnLabel, PCCLabel;
+  __label__ returnLabel;
 
   // open the libarary file
   FILE* basicLib = fopen("./libbasic.so","r");
@@ -16,7 +15,7 @@ int main(int argc, char const *argv[]) {
   }
   // go to the libarary function
   int offset = 0x106d8 - 0x10644 +  0x644;
-  const int instructions = 4; // 0x10 / 4
+  const int instructions = 7; // 0x1c / 4
   if(fseek(basicLib, offset, SEEK_SET)){
     printf("There was an error with seeking the libarary\n");
   }
@@ -39,7 +38,7 @@ int main(int argc, char const *argv[]) {
   // }
 
   // make a function pointer from it.
-  int(* __capability wrappedFunction)(int);
+  void(* __capability wrappedFunction)(void);
   __asm__ volatile (
     "cvtp %x[functionCap], %x[functionPointer] \n"
     : [functionCap] "+r" (wrappedFunction)
@@ -51,52 +50,52 @@ int main(int argc, char const *argv[]) {
   int testInt = 7;
 
   // prepare environment
-  const int functionMemSize = 0;
+  const int argumentSize = sizeof(int);
+  const int functionMemSize = argumentSize + sizeof(char* __capability);
   void* functionMemory =
-    mmap(NULL, functionMemSize + 16, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0);
+    mmap(NULL, functionMemSize, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0);
   // make capabilities
   char* __capability functionMemoryCap;
-  char* __capability returnPair[2];
+  char* __capability * __capability returnPair =
+    (__cheri_tocap char* __capability * __capability) malloc(sizeof(char*__capability)*2);
   char* returnAddress = &&returnLabel;
   char* __capability returnCap;
   __asm__ volatile (
     "cvtd %x[memCap], %x[memPointer] \n"
-    "cvtp %x[returnCap], %x[returnPointer] \n"
+    "cvtp %x[returnCap], %x[returnAddress] \n"
     : [memCap] "+r" (functionMemoryCap),
     [returnCap] "+r" (returnCap)
     : [memPointer] "r" (functionMemory),
     [returnAddress] "r" (returnAddress)
   );
-  // returnCap = __builtin_cheri_program_counter_get();
-  char* __capability contextSpaceCap[NUM_REGS];
-  returnPair[0] = contextSpaceCap;
+
+  char* __capability * __capability contextSpaceCap =
+    (__cheri_tocap char* __capability * __capability) malloc(sizeof(char*__capability)*NUM_REGS);
+
+  returnPair[0] = (char* __capability) contextSpaceCap;
   returnPair[1] = returnCap;
 
-  // store current context
-  storeContext(contextSpaceCap);
   // seal and store context capability and return capability in function mem
   __asm__ volatile (
     "seal %x[returnPair], %x[returnPair], lpb \n"
-    : [returnPair] "+r" (*returnPair)
+    : [returnPair] "+r" (returnPair)
   );
-  ((char* __capability * __capability) functionMemoryCap)[0] = returnPair;
+
+  ((char* __capability * __capability) functionMemoryCap)[0] = (char* __capability) returnPair;
   // Prepare arguments
-  // TODO
+  char* __capability argstart = functionMemoryCap + sizeof(char* __capability);
+  ((int* __capability)argstart)[0] = testInt;
+
+  // store current context
+  storeContext(contextSpaceCap);
   // clean up context and jump
-  // TODO
-  __asm__ volatile (
-    "msr ddc, %x[memCap] \n"
-    : : [memCap] "r" (functionMemoryCap)
-  );
-
-  testInt = wrappedFunction(testInt);
-
-  // read arguments
+  prepareContextAndJump(functionMemoryCap, wrappedFunction);
 
   returnLabel:
-  // restore context
-  restoreContext(contextSpaceCap);
+  // restore context needs context cap in c0.
+  restoreContext();
   // read out results
+  testInt = ((int* __capability)argstart)[0];
 
   printf("test_move is now: %d \n", testInt);
 
