cc2ea73fcc6677d642514edff309fab42c628fa9
Tom Kuchler <tom.kuchler@inf.ethz.ch>
2022-06-29 13:41:17 +0200

    added measuring to trial run

--- compartment/trial.c
+++ compartment/trial.c
@@ -1,12 +1,20 @@
-// include for printing
+// system includes
+#include <stdint.h>
 #include <stdio.h>
-#include <sys/mman.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <machine/cpu.h>
 
 #include "compartment.h"
 
 int main(int argc, char const *argv[]) {
   __label__ returnLabel;
+
+  // test arguments
+  int testInt = 7;
 
   // open the libarary file
   FILE* basicLib = fopen("./libbasic.so","r");
@@ -32,11 +40,6 @@ int main(int argc, char const *argv[]) {
   // read the aproprate number of bytes
   fread(functionCode, 4, instructions, basicLib);
 
-  // printf("instructions: \n");
-  // for(int i = 0; i < instructions; i++){
-  //   printf("%08x\n", ((int*)functionCode)[i]);
-  // }
-
   // make a function pointer from it.
   void(* __capability wrappedFunction)(void);
   __asm__ volatile (
@@ -45,43 +48,41 @@ int main(int argc, char const *argv[]) {
     : [functionPointer] "r" (functionCode)
   );
 
-
-  // call to it
-  int testInt = 7;
-
-  // prepare environment
-  const int argumentSize = sizeof(int);
-  const int functionMemSize = argumentSize + sizeof(char* __capability);
-  void* functionMemory =
-    mmap(NULL, functionMemSize, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0);
-  // make capabilities
-  char* __capability functionMemoryCap;
-  char* __capability * __capability returnPair =
-    (__cheri_tocap char* __capability * __capability) malloc(sizeof(char*__capability)*2);
+  // make a return capability
   char* returnAddress = &&returnLabel;
   char* __capability returnCap;
   __asm__ volatile (
-    "cvtd %x[memCap], %x[memPointer] \n"
     "cvtp %x[returnCap], %x[returnAddress] \n"
-    : [memCap] "+r" (functionMemoryCap),
-    [returnCap] "+r" (returnCap)
-    : [memPointer] "r" (functionMemory),
-    [returnAddress] "r" (returnAddress)
+    : [returnCap] "+r" (returnCap)
+    : [returnAddress] "r" (returnAddress)
   );
 
+  // prepare allocations
+  const int argumentSize = sizeof(int);
+  const int functionMemSize = argumentSize + sizeof(char* __capability);
+  char* __capability functionMemoryCap =
+  (__cheri_tocap char* __capability) malloc(functionMemSize);
+  char* __capability * __capability returnPair =
+  (__cheri_tocap char* __capability * __capability) malloc(sizeof(char*__capability)*2);
   char* __capability * __capability contextSpaceCap =
-    (__cheri_tocap char* __capability * __capability) malloc(sizeof(char*__capability)*NUM_REGS);
+  (__cheri_tocap char* __capability * __capability) malloc(sizeof(char*__capability)*NUM_REGS);
 
-  returnPair[0] = (char* __capability) contextSpaceCap;
-  returnPair[1] = returnCap;
+  uint64_t start, end;
+  __asm__ __volatile__("mrs %0, cntvct_el0" : "=r"(start));
+  for(int repetitions = 0; repetitions < 1000000; repetitions++){
 
-  // seal and store context capability and return capability in function mem
+  // for measurement Loop
+  char* __capability * __capability returnPairCap = returnPair;
+
+  // prepare and seal return pair
+  returnPairCap[0] = (char* __capability) contextSpaceCap;
+  returnPairCap[1] = returnCap;
   __asm__ volatile (
-    "seal %x[returnPair], %x[returnPair], lpb \n"
-    : [returnPair] "+r" (returnPair)
+    "seal %x[returnPairCap], %x[returnPairCap], lpb \n"
+    : [returnPairCap] "+r" (returnPairCap)
   );
+  ((char* __capability * __capability) functionMemoryCap)[0] = (char* __capability) returnPairCap;
 
-  ((char* __capability * __capability) functionMemoryCap)[0] = (char* __capability) returnPair;
   // Prepare arguments
   char* __capability argstart = functionMemoryCap + sizeof(char* __capability);
   ((int* __capability)argstart)[0] = testInt;
@@ -96,6 +97,17 @@ int main(int argc, char const *argv[]) {
   restoreContext();
   // read out results
   testInt = ((int* __capability)argstart)[0];
+  }
+  __asm__ __volatile__("mrs %0, cntvct_el0" : "=r"(end));
+
+  // free memory
+  free((__cheri_fromcap void*) functionMemoryCap);
+  free((__cheri_fromcap void*) returnPair);
+  free((__cheri_fromcap void*) contextSpaceCap);
+
+  printf("start %lu\n", start);
+  printf("end %lu\n", end);
+  printf("time %lu\n", end-start);
 
   printf("test_move is now: %d \n", testInt);
 
