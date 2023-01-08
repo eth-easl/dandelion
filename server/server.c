503a1e315ded849b95218d3a08f8a5149860eb3b
Georgijs Vilums <georgijs.vilums@inf.ethz.ch>
2023-01-08 14:14:06 +0100

    Rewrite server to allocate code and data separately

--- server/server.c
+++ server/server.c
@@ -56,49 +56,90 @@ int main(int argc, char const *argv[]) {
     return -1;
   }
 
-  // allocate space for the process to execute in
+  Elf_Addr max_code_addr = 0;
+
+  // unused for now
+  Elf_Addr max_data_addr = 0;
+
+  for (size_t headerIndex = 0; headerIndex < phtEntryQuantity; headerIndex++) {
+    Elf_Phdr header = phTable[headerIndex];
+    if(header.p_type == PT_LOAD){
+      Elf_Addr segment_addr_bound = header.p_vaddr + header.p_memsz;
+
+      Elf_Word flags = header.p_flags;
+      if (flags & PF_X) {
+        if (segment_addr_bound > max_code_addr) {
+          max_code_addr = segment_addr_bound;
+        }
+      } else {
+        if (segment_addr_bound > max_data_addr) {
+          max_data_addr = segment_addr_bound;
+        }
+      }
+    }
+  }
+
+  // by default, choose a large memory size so that we can fit heap and stack
   size_t memorySize = 1L<<32;
-  void* functionMemoryAddress = mmap(NULL,
-    memorySize,
+
+  void* code_ptr = mmap(NULL,
+    max_code_addr,
     PROT_EXEC | PROT_READ | PROT_WRITE,
     MAP_ANONYMOUS,
     -1,
     0
   );
-  void*__capability functionMemory =
-    (__cheri_tocap void*__capability) functionMemoryAddress;
-  functionMemory = __builtin_cheri_bounds_set(functionMemory, memorySize);
 
-  printf("functionMemory perms: %ld\n", __builtin_cheri_perms_get(functionMemory));
-  printf("functionMemory addr: %ld\n", __builtin_cheri_address_get(functionMemory));
-  printf("functionMemory base: %ld\n", __builtin_cheri_base_get(functionMemory));
-  printf("functionMemory length: %ld\n", __builtin_cheri_length_get(functionMemory));
+  void* data_ptr = mmap(NULL,
+    memorySize,
+    PROT_READ | PROT_WRITE,
+    MAP_ANONYMOUS,
+    -1,
+    0
+  );
 
-  // printf("PT_PHDR %d\n", PT_PHDR);
-  // printf("PT_LOAD %d\n", PT_LOAD);
   for (size_t headerIndex = 0; headerIndex < phtEntryQuantity; headerIndex++) {
     // process loadable headers
     // printf("first Header type %d\n", phTable[headerIndex].p_type);
-    if(phTable[headerIndex].p_type == PT_LOAD){
-      Elf_Phdr loadedHeader = phTable[headerIndex];
-      Elf_Off fileOffset = loadedHeader.p_offset;
-      Elf_Addr virtualAddress = loadedHeader.p_vaddr;
-      Elf_Word loadSize = loadedHeader.p_filesz;
-      pread(elf_file, (__cheri_fromcap void *)functionMemory+virtualAddress, loadSize, fileOffset);
+    Elf_Phdr header = phTable[headerIndex];
+    if(header.p_type == PT_LOAD){
+      Elf_Off fileOffset = header.p_offset;
+      Elf_Addr virtualAddress = header.p_vaddr;
+      Elf_Word fileSize = header.p_filesz;
+
+      Elf_Word flags = header.p_flags;
+      // if the X (exec) flag is set, map into code section, else map as data
+      void* base;
+      if (flags & PF_X) {
+        base = code_ptr;
+      } else {
+        base = data_ptr;
+      }
+      pread(elf_file, base + virtualAddress, fileSize, fileOffset);
     }
   }
 
   // make ppc
   void* __capability pcc = __builtin_cheri_program_counter_get();
-  pcc = __builtin_cheri_address_set(pcc, (unsigned long)functionMemoryAddress);
-  pcc = __builtin_cheri_bounds_set(pcc, memorySize);
+  pcc = __builtin_cheri_address_set(pcc, (unsigned long)code_ptr);
+  pcc = __builtin_cheri_bounds_set(pcc, max_code_addr);
   pcc = pcc + entryPoint;
-  printf("pcc perms: %ld\n", __builtin_cheri_perms_get(pcc));
-  printf("pcc addr: %ld\n", __builtin_cheri_address_get(pcc));
-  printf("pcc base: %ld\n", __builtin_cheri_base_get(pcc));
+
+  void*__capability ddc =
+    (__cheri_tocap void*__capability) data_ptr;
+  ddc = __builtin_cheri_bounds_set(ddc, memorySize);
+
+  printf("pcc perms : %ld\n", __builtin_cheri_perms_get(pcc));
+  printf("pcc addr  : %ld\n", __builtin_cheri_address_get(pcc));
+  printf("pcc base  : %ld\n", __builtin_cheri_base_get(pcc));
   printf("pcc length: %ld\n", __builtin_cheri_length_get(pcc));
 
-  sandboxedCall(pcc, functionMemory, (void*) memorySize -16);
+  printf("ddc perms : %ld\n", __builtin_cheri_perms_get(ddc));
+  printf("ddc addr  : %ld\n", __builtin_cheri_address_get(ddc));
+  printf("ddc base  : %ld\n", __builtin_cheri_base_get(ddc));
+  printf("ddc length: %ld\n", __builtin_cheri_length_get(ddc));
+
+  sandboxedCall(pcc, ddc, (void*) memorySize - 16);
 
   printf("Server Goodbye\n");
 
