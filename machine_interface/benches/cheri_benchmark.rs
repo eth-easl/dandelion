68373d1fb8486990d2c04e2f0c546601720c2f5e
Tom Kuchler <tom.kuchler@inf.ethz.ch>
2023-04-21 10:04:32 +0200

    Add first few microbenchmarks for cheri

---
+++ machine_interface/benches/cheri_benchmark.rs
@@ -0,0 +1,109 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use machine_interface::{
+    function_lib::{
+        cheri::{CheriDriver, CheriLoader},
+        util::load_static,
+        Driver, Engine, Loader,
+    },
+    memory_domain::{cheri::CheriMemoryDomain, ContextTrait, MemoryDomain},
+    DataItem, DataItemType, Position,
+};
+
+fn context_benchmark(c: &mut Criterion) {
+    let mut domain =
+        CheriMemoryDomain::init(Vec::<u8>::new()).expect("Should be able to initialize");
+    let mut group = c.benchmark_group("cheri context aquire and release");
+    static KB: usize = 1024;
+    for size in [128 * KB, 2 * KB * KB].iter() {
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, i| {
+            b.iter(|| {
+                let context = domain
+                    .acquire_context(*i)
+                    .expect("Should be able to allocate");
+                domain
+                    .release_context(context)
+                    .expect("Should be able to deallocate");
+            })
+        });
+    }
+    group.finish();
+}
+
+fn matmul_sequential_benchmark(c: &mut Criterion) {
+    const MAT_SIZE: usize = 128;
+
+    let mut domain =
+        CheriMemoryDomain::init(Vec::<u8>::new()).expect("Should be able to initialize");
+    let mut driver = CheriDriver::new(vec![1]).expect("Should be able to create driver");
+    let mut engine = driver
+        .start_engine()
+        .expect("Should have gotten one engine");
+    let mut path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push("tests/data/test_elf_aarch64c_matmul");
+    let mut elf_file = std::fs::File::open(path).expect("Should have found test file");
+    let mut elf_buffer = Vec::<u8>::new();
+    use std::io::Read;
+    elf_file
+        .read_to_end(&mut elf_buffer)
+        .expect("Should be able to read entire file");
+    let (req_list, mut static_context, config) =
+        CheriLoader::parse_function(elf_buffer, &mut domain).expect("Should success at parsing");
+    c.bench_function("matmul", |b| {
+        b.iter(|| {
+            let mut function_context = load_static(&mut domain, &mut static_context, &req_list)
+                .expect("Should be able to configure function context");
+            // add inputs
+            let in_size_offset = function_context
+                .get_free_space(8, 8)
+                .expect("Should have space for single i64");
+            function_context
+                .write(in_size_offset, i64::to_ne_bytes(MAT_SIZE as i64).to_vec())
+                .expect("Write should go through");
+            function_context.dynamic_data.push(DataItem {
+                index: 0,
+                item_type: DataItemType::Item(Position {
+                    offset: in_size_offset,
+                    size: 8,
+                }),
+            });
+            let input_size = 8 * MAT_SIZE * MAT_SIZE;
+            let in_mat_offset = function_context
+                .get_free_space(input_size, 8)
+                .expect("Should have space for single i64");
+            let mut mat_vec = Vec::<u8>::new();
+            for i in 0..(MAT_SIZE * MAT_SIZE) {
+                mat_vec.append(&mut i64::to_ne_bytes(i as i64).to_vec());
+            }
+            function_context
+                .write(in_mat_offset, mat_vec)
+                .expect("Write should go through");
+            function_context.dynamic_data.push(DataItem {
+                index: 1,
+                item_type: DataItemType::Item(Position {
+                    offset: in_mat_offset,
+                    size: input_size,
+                }),
+            });
+            let out_mat_offset = function_context
+                .get_free_space(input_size, 8)
+                .expect("Should have space for single i64");
+            function_context.dynamic_data.push(DataItem {
+                index: 2,
+                item_type: DataItemType::Item(Position {
+                    offset: out_mat_offset,
+                    size: input_size,
+                }),
+            });
+            let (result, result_context) = tokio::runtime::Builder::new_current_thread()
+                .build()
+                .unwrap()
+                .block_on(engine.run(&config, function_context));
+            domain.release_context(result_context);
+        })
+    });
+}
+
+async fn run_matmul() {}
+
+criterion_group!(benches, context_benchmark, matmul_sequential_benchmark);
+criterion_main!(benches);
