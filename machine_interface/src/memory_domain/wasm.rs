626ae9f240987f4a1ec4907548ff12c0aea7540c
Leon Thomm <l.thomm@mailbox.org>
2024-03-19 19:17:55 +0100

    fix merge things

--- machine_interface/src/memory_domain/wasm.rs
+++ machine_interface/src/memory_domain/wasm.rs
@@ -3,65 +3,6 @@ use crate::util::mmapmem::MmapMem;
 use dandelion_commons::{DandelionError, DandelionResult};
 use nix::sys::mman::ProtFlags;
 
-mod wasm_memory_allocation {
-    use dandelion_commons::DandelionError;
-    use libc::{mmap, munmap};
-    use std::ops::Deref;
-    use std::ops::DerefMut;
-
-    /// A smart pointer for memory mapped memory.
-    /// It makes sure that the memory is unmapped when 
-    /// it is dropped, and prevents Rust from trying to
-    /// free the memory through the global allocator.
-    #[derive(Debug)]
-    pub struct MmapBox {
-        ptr: *mut u8,
-        size: usize,
-    }
-
-    impl MmapBox {
-        pub fn new(size: usize) -> Result<MmapBox, DandelionError> {
-            let addr = unsafe { mmap(
-                0 as *mut _,
-                size,
-                libc::PROT_READ | libc::PROT_WRITE,
-                libc::MAP_PRIVATE | libc::MAP_ANONYMOUS,
-                -1,
-                0,
-            ) };
-            match addr {
-                libc::MAP_FAILED => {
-                    return Err(DandelionError::OutOfMemory);
-                },
-                _ => {}
-            }
-            Ok(MmapBox { ptr: addr as *mut u8, size })
-        }
-    }
-
-    impl Deref for MmapBox {
-        type Target = [u8];
-
-        fn deref(&self) -> &Self::Target {
-            unsafe { std::slice::from_raw_parts(self.ptr, self.size) }
-        }
-    }
-
-    impl DerefMut for MmapBox {
-        fn deref_mut(&mut self) -> &mut Self::Target {
-            unsafe { std::slice::from_raw_parts_mut(self.ptr, self.size) }
-        }
-    }
-
-    impl Drop for MmapBox {
-        fn drop(&mut self) {
-            unsafe { munmap(self.ptr as *mut _, self.size); }
-        }
-    }
-}
-
-use wasm_memory_allocation::MmapBox;
-
 pub static WASM_PAGE_SIZE: usize = 64 * 1024;                       // 64KiB
 pub static MAX_WASM_MEMORY_SIZE: usize = 4 * 1024 * 1024 * 1024;    // 4GiB
 
