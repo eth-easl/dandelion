e53c2a0ab53c9db63069e538857afdd0feb5d33a
jonathanplsmith <92918075+jonathanplsmith@users.noreply.github.com>
2024-05-30 11:23:20 +0200

    Revert "do transfer in spawn_blocking"
    This reverts commit 84ba4d24e6f69d229c5face6699b3f2a4d994e08.

--- dandelion_commons/src/lib.rs
+++ dandelion_commons/src/lib.rs
@@ -48,8 +48,6 @@ pub enum DandelionError {
     TransferInputNoSetAvailable,
     /// tried to transfer to a data item that was already present
     TransferItemAlreadyPresent,
-    /// transfer thread crashed (gpu_process only)
-    TransferThreadError,
     /// error converting pointers or integers
     UsizeTypeConversionError,
     /// context synchronization failed
