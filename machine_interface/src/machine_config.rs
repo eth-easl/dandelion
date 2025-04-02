8f04f353e3670c37ba68760a4f453234ceeae1e7
Tobias Stocker <tbs.stk@bluewin.ch>
2025-04-02 11:13:03 +0200

    Changed default context size for HTTP function to 512 MiB

--- machine_interface/src/machine_config.rs
+++ machine_interface/src/machine_config.rs
@@ -50,7 +50,7 @@ pub fn get_compatibilty_table() -> BTreeMap<EngineType, DomainType> {
 }
 
 #[cfg(any(feature = "reqwest_io"))]
-const SYS_FUNC_DEFAULT_CONTEXT_SIZE: usize = 0x400_0000; // 64 MiB
+const SYS_FUNC_DEFAULT_CONTEXT_SIZE: usize = 0x2000_0000; // 512 MiB
 
 pub fn get_system_functions(engine_type: EngineType) -> Vec<(SystemFunction, usize)> {
     return match engine_type {
