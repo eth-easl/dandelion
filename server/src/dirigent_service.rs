f30317c8a542d715b37910bf9623ce27ef32794b
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2025-12-05 17:33:02 +0100

    Code refactoring and fixed endpoint removal
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- server/src/dirigent_service.rs
+++ server/src/dirigent_service.rs
@@ -232,15 +232,6 @@ async fn create_dirigent_server(
 
     tracing::info!("Listening on {}", addr);
 
-    // TMP: just for initial testing, should be removed
-    //process_add_action(Arc::clone(&dirigent_service.data), String::from("warm-function-4949985443906962520"), String::from("111"), String::from("localhost:5555"));
-    process_add_action(
-        Arc::clone(&dirigent_service.data),
-        String::from("warm_function"),
-        String::from("sandbox_222"),
-        String::from("localhost:4567"),
-    );
-
     loop {
         let (stream, _) = listener.accept().await?;
         let io = hyper_util::rt::TokioIo::new(stream);
