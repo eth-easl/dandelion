eefa4fe7412150554cb2bbb827e3d2773dc882c9
Tom Kuchler <tom.j.kuchler@gmail.com>
2026-07-17 15:04:01 +0200

    Try parallelize remote item conversion

--- dispatcher/src/dispatcher.rs
+++ dispatcher/src/dispatcher.rs
@@ -568,10 +568,16 @@ impl Dispatcher {
             // Defer actual execution of system functions (i.e. fetching),
             // by calling the system function to produce a composition set containing the reference to be resolved later
             FunctionType::SystemFunction(sys_function) => {
-                machine_interface::function_driver::system_driver::convert_to_references(
-                    sys_function,
-                    input_sets,
-                )
+                // need to spawn here, as large sets may otherwise block for a long time
+                // TODO: decide if we need tokio spawn here / if we want to keep it anyway or if we should try to find a runtime agnostic spawn.
+                tokio::task::spawn(async move {
+                    machine_interface::function_driver::system_driver::convert_to_references(
+                        sys_function,
+                        input_sets,
+                    )
+                })
+                .await
+                .unwrap()
             }
             FunctionType::Function(func_info) => {
                 let function_alternatives = func_info
